ea7ac09f099b8ed6f12b53d8ccac7fc76d000f2c
Yogev Rahamim <yogev26r@gmail.com>
2020-06-06 20:07:00 +0300

    Added Covid-19 trigger

--- HamiltonianRide/Assets/Scripts/Heart.cs
+++ HamiltonianRide/Assets/Scripts/Heart.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Heart : MonoBehaviour
 {
-    [Tooltip("The velocity of the object, in meters per second")] [SerializeField] float speed = 10f;
-    // Start is called before the first frame update
+    [Tooltip("The velocity of the object, in meters per second")]
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    GameManager gm;
+
     void Start()
     {
         Transform t = GetComponent<Transform>();
 
     }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
         transform.localScale = new Vector3(transform.localScale.x + speed, transform.localScale.y + speed, transform.localScale.z);
         if ((Mathf.Abs(transform.localScale.x) < -400f) && ((transform.localScale.x) < -200f) && (speed > 0f))
@@ -35,5 +39,12 @@ public class Heart : MonoBehaviour
         else if ((Mathf.Abs(transform.localScale.x) < 20f) && ((transform.localScale.x) < 0f) && (speed < 0f))
             speed = 0.001f;
 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.GameOver(0);
+        }
     }
 }
