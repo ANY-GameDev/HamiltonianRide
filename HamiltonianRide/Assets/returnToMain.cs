95fae79739ffc7973a294af8b538e5544cd18683
nathane123 <54856816+nathane123@users.noreply.github.com>
2020-06-08 23:46:22 +0300

    Add menu and component

---
+++ HamiltonianRide/Assets/returnToMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rerurnToMain : MonoBehaviour
+{
+    public void returnMain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
